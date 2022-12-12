507b735000935f984ea6db243e2286c491a8b602
Duc Phu <vandungqc03@gmail.com>
2022-12-12 19:10:39 +0700

    Removed submit search form and changed to debounced search form

--- client/src/components/SearchFormUser.js
+++ client/src/components/SearchFormUser.js
@@ -1,45 +1,57 @@
 import FormRow from './FormRow'
 import Form from 'react-bootstrap/Form'
+import useDebounce from '../hooks/useDebounce'
 import { useAppContext } from '../context/appContext'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 const SearchFormUser = () => {
   const [searchName, setSearchName] = useState({ name: '' })
-  const { getSearchProductInput, search, changePage } = useAppContext()
+  const refContainer = useRef(true)
+  const { getSearchProductInput, changePage, search } = useAppContext()
 
   const handleChange = (e) => {
     setSearchName({ ...searchName, name: e.target.value })
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    
+  const debounceSearch = useDebounce(searchName.name, 800)
+
+  useEffect(() => {
+    const isFirstRender = cancelTheFirstRender()
+    if (isFirstRender) return
+
     const query = {
-      price: search.price || 50,
-      category: search.category || 'all',
-      name: searchName.name,
+      name: debounceSearch,
+      price: search.price,
+      category: search.category,
     }
 
+    console.log(query)
     const page = 1
     changePage(page)
     getSearchProductInput(query)
+  }, [debounceSearch])
+
+  const cancelTheFirstRender = () => {
+    if (refContainer.current) {
+      refContainer.current = false
+      return true
+    } else {
+      return false
+    }
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <div className='d-flex align-items-center'>
-        <div className='flex-fill'>
-          <FormRow
-            type='text'
-            name='name'
-            value={searchName.name}
-            handleChange={handleChange}
-            placeholder='Search for your favorite books!'
-          />
-        </div>
-        <button className='btn btn-primary product-search-btn'>Search</button>
+    <div className='d-flex align-items-center'>
+      <div className='flex-fill'>
+        <FormRow
+          type='text'
+          name='name'
+          value={searchName.name}
+          handleChange={handleChange}
+          placeholder='Search here!'
+        />
       </div>
-    </Form>
+    </div>
   )
 }
 export default SearchFormUser

068a42a90978665b60b85ae22bad648c1b1213c7
Duc Phu <vandungqc03@gmail.com>
2022-12-12 18:00:58 +0700

    Added genre options object as utils

---
+++ client/src/utils/genres.js
@@ -0,0 +1,10 @@
+export const genresOptions = [
+  { id: 1, genre: 'all' },
+  { id: 2, genre: 'classic' },
+  { id: 3, genre: 'mystery' },
+  { id: 4, genre: 'fantasy' },
+  { id: 5, genre: 'historical' },
+  { id: 6, genre: 'horror' },
+  { id: 7, genre: 'sci-fi' },
+  { id: 8, genre: 'adventure' },
+]

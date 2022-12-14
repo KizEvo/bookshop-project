d95ae9ad273d2feaf8585a242a2c3b5b8023c08d
Duc Phu <vandungqc03@gmail.com>
2022-12-14 17:31:29 +0700

    Added more css and decoration

--- client/src/components/ProductContainerUser.js
+++ client/src/components/ProductContainerUser.js
@@ -1,14 +1,26 @@
 import SearchFormUser from './SearchFormUser'
 import ProductDisplay from './ProductDisplay'
 import SmallFilterForm from './SmallFilterForm'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { useAppContext } from '../context/appContext'
 
 const ProductContainerUser = () => {
+  const { totalProducts } = useAppContext()
+
   return (
     <div className='product-container-user'>
       <div className='form-background p-2'>
         <SearchFormUser />
       </div>
-      <SmallFilterForm />
+      <div className='d-flex justify-content-between align-items-center'>
+        <div className='d-flex align-items-center'>
+          <p className='total-products my-2'>{totalProducts} Products</p>
+          <AiOutlineShoppingCart className='icon my-2' />
+        </div>
+        <hr className='d-md-none w-25 flex-grow-1' />
+        <hr className='d-none d-md-block w-100' />
+        <SmallFilterForm />
+      </div>
       <ProductDisplay />
     </div>
   )
